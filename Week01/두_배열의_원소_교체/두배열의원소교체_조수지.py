ebe0a58d29bf2b7792b2cdf9182bfb69207738ce
suji <csj1430@naver.com>
2024-06-29 17:11:55 +0900

    Refactor: 정렬 이용한 코드 추가

--- Week01/두_배열의_원소_교체/두배열의원소교체_조수지.py
+++ Week01/두_배열의_원소_교체/두배열의원소교체_조수지.py
@@ -1,3 +1,5 @@
+"""
+#첫번째 시도 (정렬을 안함)
 n, k = map(int, input().split())  # n개의 원소, 최대 k번까지 바꿔치기 연산 가능
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
@@ -17,4 +19,20 @@ for _ in range(k):
     else:
         break
 
+print(sum(a))
+"""
+
+#두번째 시도 (정렬o)
+n, k = map(int, input().split())  # n개의 원소, 최대 k번까지 바꿔치기 연산 가능
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+# a는 오름차, b는 내림차순 정렬
+a.sort()
+b.sort(reverse=True)
+
+for i in range(k) : 
+    if a[i] < b[i] : # a[i]가 b[i]보다 작은 값이면 교체해 줌
+        a[i], b[i] = b[i], a[i]
+        
 print(sum(a))
