bb9e8bfb4cf669cbde79a59cfbe1a19479af96f1
CHO <imalex12@naver.com>
2024-07-10 23:28:42 +0900

    add: first

--- Week02/Main.java
+++ Week02/Main.java
@@ -1,5 +1,7 @@
 import java.io.*;
+import java.util.*;
 
+/*
 public class Main {
     public int ground(int n, int[] nList){
         if(n==1){
@@ -29,4 +31,105 @@ public class Main {
         int result = m.ground(n,nList);
         System.out.println(result);
     }
+}
+/*
+
+public class Main{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[] NM = Arrays.stream(br.readLine().split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+
+    List<int[]> graph = new ArrayList<>();
+    for(int i =0;i<NM[0];i++){
+        row = Arrays.stream(br.readLine().split())
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        graph.append(row);
+    }
+
+    public static int dfs(int x, int y){
+        if(x<0 || x>NM[0] || y<0 || y>NM[1]){
+            return 0;
+        }if(graph[x][y]==0){
+            graph[x][y] =1;
+            dfs(x+1,y);
+            dfs(x-1,y);
+            dfs(x,y+1);
+            dfs(x,y-1);
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    int result = 0;
+    for(int i =0;i<NM[0];i++){
+        for(int j=0; j<NM[1];j++){
+            if(graph[i][j] == 0){
+                dfs(i,j);
+                result++;
+            }
+        }
+    }System.out.println(result);
+
+    bw.close();
+    br.close();
+
+
+} */
+
+
+public class Main{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[] NM = Arrays.stream(br.readLine().split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+
+    List<int[]> graph = new ArrayList<>();
+    for(int i =0;i<NM[0];i++){
+        int[] row = Arrays.stream(br.readLine().split())
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        graph.append(row);
+    }
+
+    public static int dfs(int x, int y){
+        if(x<0 || x>NM[0] || y<0 || y>NM[1]){
+            return 0;
+        }if(graph[x][y]==0){
+            graph[x][y] =1;
+            dfs(x+1,y);
+            dfs(x-1,y);
+            dfs(x,y+1);
+            dfs(x,y-1);
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int result = 0;
+        for(int i =0;i<NM[0];i++){
+            for(int j=0; j<NM[1];j++){
+                if(graph[i][j] == 0){
+                    dfs(i,j);
+                    result++;
+                }
+            }
+        }System.out.println(result);
+
+        bw.close();
+        br.close();
+    }
+
+
+
 }
