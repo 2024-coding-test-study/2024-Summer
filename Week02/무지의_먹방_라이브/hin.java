edb670881ab58e72eb90e29cb25aabdd33b768b7
CHO <imalex12@naver.com>
2024-07-14 18:17:30 +0900

    add: first

---
+++ Week02/무지의_먹방_라이브/hin.java
@@ -0,0 +1,29 @@
+//실패
+import java.util.LinkedList;
+class Solution {
+    public static LinkedList<Integer> convertArrayToLinkedList(int[] array) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int num : array) {
+            linkedList.add(num);
+        }
+        return linkedList;
+    }
+    public int solution(int[] food_times, long k) {
+        LinkedList<Integer> linkedList = convertArrayToLinkedList(food_times);
+
+        int foodIdx = 0;
+        for(int i =0;i<k;i++){
+            if(foodIdx>=linkedList.size()){
+                foodIdx=0;
+            }if(linkedList.size()==0){
+                return -1;
+            }if(linkedList.get(foodIdx)!=0){
+                linkedList.set(foodIdx,linkedList.get(foodIdx)-1);
+                foodIdx++;
+            }else{
+                linkedList.remove(foodIdx);
+            }
+        }
+        return foodIdx;
+    }
+}
