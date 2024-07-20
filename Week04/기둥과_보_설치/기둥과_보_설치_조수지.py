98d5947bcedcfa57dc12b26531257d6fb4a61883
suji <csj1430@naver.com>
2024-07-20 17:08:46 +0900

    Refactor: 실행 가능하게 수정
    답지와 거의 동일함 ㅜㅜ

--- Week04/기둥과_보_설치/기둥과_보_설치_조수지.py
+++ Week04/기둥과_보_설치/기둥과_보_설치_조수지.py
@@ -1,31 +1,40 @@
-# 2차원 배열로 어떻게든 해결해보고 싶었지만 실패...
-# wall 배열 하나로만 표현할 수가 없는데...
-# 두 개 이상의 배열을 사용해야하는 거라면 어떻게 구현하는것이냐... 으앙아아ㅏ아
+# 현재 설치된 구조물이 '가능한' 구조물인지 확인하는 함수
+def possible(wall) :
+    for x, y, a in wall :
+        
+        if a == 0: # 설치된 것이 '기둥'인 경우
+            # '바닥 위' 혹은 '보의 한쪽 끝 부분 위' 혹은 '다른 기둥 위'라면 정상
+            if y == 0 or [x - 1, y, 1] in wall or [x, y, 1] in wall or [x, y - 1, 0] in wall:
+                continue
+            
+            return False # 설치될 수 없는 구조물이 설치되었다면 거짓을 반환
+        
+        elif a == 1 : # 설치된 것이 '보'인 경우
+            # '한쪽 끝부분이 기둥 위' 혹은 '양쪽 끝부분이 다른 보와 동시에 연결'이라면 정상
+            if [x, y - 1, 0] in wall or [x + 1, y - 1, 0] in wall or ([x - 1, y, 1] in wall and [x + 1, y, 1] in wall):
+                continue
+            
+            return False # 설치될 수 없는 구조물이 설치되었다면 거짓을 반환
+        
+    return True
 
-def solution(n, build_frame):
-    wall = [[0] * (n + 1) for _ in range(n + 1)]  # 보드 초기화
+def solution(n, build_frame) :
+    wall = [] # 현재 벽 상태를 나타낼 2차원 배열
     
     for i in range(len(build_frame)) :
-        x, y, a, b = build_frame[i] # build_frame 리스트의 i번째 요소를 각각 지정해줌
+        x, y, a, b = build_frame[i]
         
         if b == 1 : # 설치하는 경우
-            if a == 0 : # 기둥을 설치하는 경우
-                wall[x][y] 
-                ...
-                
-            else : # 보를 설치하는 경우
-                wall[x][y]
-                ...
+            wall.append([x, y, a]) # 일단 설치
+            
+            if not possible(wall) : # 가능한 구조물인지 확인
+                wall.remove([x, y, a]) # 가능한 구조물이 아니라면 다시 제거
         
-        else : # 삭제하는 경우
-            if a == 0 : # 기둥을 삭제하는 경우
-                wall[x][y]
-                ...
+        if b == 0 : # 삭제하는 경우
+            wall.remove([x, y, a]) # 일단 삭제
+            
+            if not possible(wall) : # 가능한 구조물인지 확인
+                wall.append([x, y, a]) # 가능한 구조물이 아니라면 다시 설치
                 
-            else : # 보를 삭제하는 경우
-                wall[x][y]
-                ...
-    
-    answer = [[]]
-    
-    return answer
+                
+    return sorted(wall) # 정렬된 결과를 반환
