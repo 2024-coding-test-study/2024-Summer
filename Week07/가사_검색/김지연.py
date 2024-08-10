9ef6832d7aec0bb9e273fc8ca906f5e54d56a5d7
xxyeon <jykim9335@gmail.com>
2024-08-11 01:15:07 +0900

    Refactor: 중복 코드 제거, binary_search함수에서 words 길이 검사 제거

--- Week07/가사_검색/김지연.py
+++ Week07/가사_검색/김지연.py
@@ -1,11 +1,6 @@
 def binary_search(start, end, target, words):
     #target이 정렬된 words 어디에 위치해야하는지 탐색
-    if len(words) == 1:
-        if words[0] < target:
-            start = 1
-        elif words[0] > target:
-            start = 0
-        return start
+
     while start < end:
         mid = (start + end) // 2
 
@@ -27,12 +22,12 @@ def solution(words, queries):
     words_reversed.sort()
 
     #길이에 따라 리스트를 따로 두어서 비교해야함!!!
-    words_length = [[] for i in range(100000)]
+    words_length = [[] for i in range(10001)]
     for word in words:
         words_length[len(word)].append(word)
     #접미사 탐색을 위한 word reverse 리스트 만들기
     
-    words_reverse = [[] for i in range(100000)]
+    words_reverse = [[] for i in range(10001)]
     for word in words_reversed:
         words_reverse[len(word)].append(word)
     
@@ -40,20 +35,21 @@ def solution(words, queries):
     for target in queries:
         #이렇게하는건 전치사일 경우 상관없는데 접두사일 경우는...? 접두사는 문자열 뒤집어서 탐색
         if target[0] == '?': #접미사이면
-            left_target = target.replace('?', 'a') #left = left_target이 words에서 위치한 인덱스 +1
-            right_target = target.replace('?', 'z') #right = right_tartget이 words에서 위치한 인덱스
-            left_idx = binary_search(0, len(words_reverse[len(target)])-1, left_target[::-1], words_reverse[len(target)])
-            right_idx = binary_search(0, len(words_reverse[len(target)])-1, right_target[::-1], words_reverse[len(target)]) + 1
+            left_target = target.replace('?', 'a')[::-1] #left = left_target이 words에서 위치한 인덱스 +1
+            right_target = target.replace('?', 'z')[::-1] #right = right_tartget이 words에서 위치한 인덱스
+            word_search = words_reverse[len(target)]
+            # left_idx = binary_search(0, len(words_reverse[len(target)])-1, left_target[::-1], words_reverse[len(target)])
+            # right_idx = binary_search(0, len(words_reverse[len(target)])-1, right_target[::-1], words_reverse[len(target)]) + 1
         else:
             left_target = target.replace('?', 'a') #left = left_target이 words에서 위치한 인덱스 +1
             right_target = target.replace('?', 'z') #right = right_tartget이 words에서 위치한 인덱스
-        
+            word_search = words_length[len(target)]
         #right - left하면 words안에 target수를 구할 수 있다.
         # left, right 각각 이진탐색 시작
-            left_idx = binary_search(0, len(words_length[len(target)])-1, left_target, words_length[len(target)])
-            right_idx = binary_search(0, len(words_length[len(target)])-1, right_target, words_length[len(target)]) + 1
+        left_idx = binary_search(0, len(word_search[len(target)])-1, left_target, word_search)
+        right_idx = binary_search(0, len(word_search[len(target)])-1, right_target, word_search)
 
-        answer.append(right_idx - left_idx - 1)
+        answer.append(right_idx - left_idx)
         
                 
     return answer
