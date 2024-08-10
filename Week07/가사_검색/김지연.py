4027f9628e05c2d0181d017c98399c96fad6d6e3
xxyeon <jykim9335@gmail.com>
2024-08-11 00:47:47 +0900

    Refactor: 가사 검색.py 입출력 예 통과, 테케 모두 실패...

--- Week07/가사_검색/김지연.py
+++ Week07/가사_검색/김지연.py
@@ -1,54 +1,59 @@
-def binary_search(start, end, target, count, words):
-  word = target[0]
-  start_idx = target[1]
-  length = target[2]
-  while True:
-    if start > end:
-      return count
-      # print(count)
+def binary_search(start, end, target, words):
+    #target이 정렬된 words 어디에 위치해야하는지 탐색
+    if len(words) == 1:
+        if words[0] < target:
+            start = 1
+        elif words[0] > target:
+            start = 0
+        return start
+    while start < end:
+        mid = (start + end) // 2
 
-
-    mid = (start + end) // 2
-   
- 
-    if word == str(words[mid][start_idx:start_idx+len(word)]):
-        if length == len(words[mid]):
-            print(start, end, mid)
-            
-            count += 1
-            start += 1
-          
-            # print(words[mid][start_idx:start_idx+len(word)])
-        elif length < len(words[mid]):
-            end = mid - 1
-        elif length > len(words[mid]):
+        if words[mid] < target:
             start = mid + 1
+        elif words[mid] > target:
+            end = mid #mid-1을 하지 않는 이유는 target이 words[mid]보다 작을경우 target은 words의 mid에 위치해야한다. 그래야 words[mid]의 값이 target뒤에 위치하게 된다.
+    return start #start가 end보다 커지면 탐색을 모두 마친것이고 start의 위치가 target이 words에 위치할 인덱스가 된다.
+    
+            
 
-    elif word < str(words[mid][start_idx:start_idx+len(word)]): #두 
-        # print(end)
-        end = mid - 1
-        
-
-    elif word > str(words[mid][start_idx:start_idx+len(word)]):
-        # print(start)
-        start = mid + 1
-
-
-
+    
 def solution(words, queries):
-  answer = []
-  keyword = []
-  words.sort()
-  print(words)
-  for i in queries:
-      to_find = i.split("?")
-      for k in range(len(to_find)):
-          if to_find[k] != '':
-              keyword.append((to_find[k], k, len(i)))
+    answer = []
+    words.sort()
+    words_reversed = []
+    for word in words:
+        words_reversed.append(word[::-1])
+    words_reversed.sort()
 
-  for i in keyword:
-      cnt = 0
-      cnt = binary_search(0, len(words)-1, i, cnt, words)
-      answer.append(cnt)
+    #길이에 따라 리스트를 따로 두어서 비교해야함!!!
+    words_length = [[] for i in range(100000)]
+    for word in words:
+        words_length[len(word)].append(word)
+    #접미사 탐색을 위한 word reverse 리스트 만들기
+    
+    words_reverse = [[] for i in range(100000)]
+    for word in words_reversed:
+        words_reverse[len(word)].append(word)
+    
+    #queries에서 ?를 a와 z로 바꾸고 각각 탐색
+    for target in queries:
+        #이렇게하는건 전치사일 경우 상관없는데 접두사일 경우는...? 접두사는 문자열 뒤집어서 탐색
+        if target[0] == '?': #접미사이면
+            left_target = target.replace('?', 'a') #left = left_target이 words에서 위치한 인덱스 +1
+            right_target = target.replace('?', 'z') #right = right_tartget이 words에서 위치한 인덱스
+            left_idx = binary_search(0, len(words_reverse[len(target)])-1, left_target[::-1], words_reverse[len(target)])
+            right_idx = binary_search(0, len(words_reverse[len(target)])-1, right_target[::-1], words_reverse[len(target)]) + 1
+        else:
+            left_target = target.replace('?', 'a') #left = left_target이 words에서 위치한 인덱스 +1
+            right_target = target.replace('?', 'z') #right = right_tartget이 words에서 위치한 인덱스
+        
+        #right - left하면 words안에 target수를 구할 수 있다.
+        # left, right 각각 이진탐색 시작
+            left_idx = binary_search(0, len(words_length[len(target)])-1, left_target, words_length[len(target)])
+            right_idx = binary_search(0, len(words_length[len(target)])-1, right_target, words_length[len(target)]) + 1
 
-  return answer
+        answer.append(right_idx - left_idx - 1)
+        
+                
+    return answer
