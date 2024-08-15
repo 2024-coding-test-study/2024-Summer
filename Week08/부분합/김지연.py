3fe2f3afb6c41461694c6fae9633ac3795f97cc4
xxyeon <jykim9335@gmail.com>
2024-08-16 02:21:48 +0900

    Refactor: 부분합.py 해결 for 이중 반복문 -> while로 변경

--- Week08/부분합/김지연.py
+++ Week08/부분합/김지연.py
@@ -5,34 +5,25 @@ n, s = map(int, sys.stdin.readline().rstrip().split())
 data = list(map(int, sys.stdin.readline().rstrip().split()))
 
 start, end = 0, 0
-interval = n
-p = False
+interval = n + 1
+current_sum = 0
+
+while start < n and end <= n:
+  
+  if current_sum >= s:
+    #구간 줄이기
+    interval = min(interval, end - start) #end 포인터 전까지
+    current_sum -= data[start]
+    start += 1
+    
+  elif current_sum < s:
+    if end >= n:
+      break
+    current_sum += data[end]
+    end += 1
 
 
-for start in range(n):
-  total = 0
-  end = start
-  current_sum = data[start]
-
-
-  for i in range(start, n):
-    if current_sum >= s:
-      
-      
-      p = True
-      if interval > end - start + 1:
-        interval = end - start + 1
-        break
-    elif current_sum < s:
-      end += 1
-      if end >= n:
-        break
-      current_sum += data[end]
-  if interval == 1:
-    break
-
-
-if p:
+if interval < n+1:
   print(interval)
 else:
   print(0)
